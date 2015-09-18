62b003b02d260daf2d2a714b8a7167b356c6692f
ChrissMaacc <Chris.McDonald12@outlook.com>
2015-09-18 13:54:54 -0400

    Added The Maps Function

--- javascript/mapTools.js
+++ javascript/mapTools.js
@@ -1,20 +1,7 @@
-function initBasicMap(lat, long){
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: lat, long: long},
-		scrollwheel: false,
-		zoom: 8					//TODO determine preferable zoom.
-	});
-}//initBasicMap()
 
-function initDirectionsMap(lat1, long1, lat2, long2){
+function initDirections(lat1, long1, lat2, long2, map){
 	var location1 = {lat: lat1, long: long1};
 	var location2 = {lat: lat2, long: long2};
-
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: lat1, long: long1},	//TODO compute center between locations
-		scrollwheel: false,
-		zoom: 8					//TODO determine preferable zoom.
-	});
 	
 	var directionsService = new google.maps.DirectionsService();
 	var directionDisplay = new google.maps.DirectionRenderer({map: map});
